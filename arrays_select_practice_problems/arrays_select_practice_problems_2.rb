2c8a0fb76816681050e6f74f1972c7724b1323f3
ryan <ryanwaszak@gmail.com>
2020-03-09 23:21:04 -0500

    add arrays_select_practice_problems_2.rb

---
+++ arrays_select_practice_problems/arrays_select_practice_problems_2.rb
@@ -0,0 +1,64 @@
+#  1. Start with an array of numbers and create a new array with only the numbers less than 20.
+#     For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
+
+# numbers = [2, 32, 80, 18, 12, 3]
+
+# smaller = numbers.select{|number| number < 20 }
+
+# p smaller 
+
+#  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
+#     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
+
+# strings = ["winner", "winner", "chicken", "dinner"] 
+
+# p start_with_w = strings.select{|string| string[0].downcase == "w"}
+
+#  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
+
+# items = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] 
+# p greater_than_five = items.select{|item| item[:price] > 5}
+
+#  4. Start with an array of numbers and create a new array with only the even numbers.
+#     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
+# numbers = [2, 4, 5, 1, 8, 9, 7]
+# p even = numbers.select{|number| number.even?}
+
+#  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
+#     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
+# strings = ["a", "man", "a", "plan", "a", "canal", "panama"]
+# p short_length = strings.select{|word| word.length < 4}
+
+#  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] 
+# becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
+# items = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] 
+# p short_names = items.select{|item| item[:name].length < 6}
+
+#  7. Start with an array of numbers and create a new array with only the numbers less than 10.
+#     For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
+# numbers = [8, 23, 0, 44, 1980, 3]
+# p less_than_ten = numbers.select{|number| number < 10}
+
+
+#  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
+#     For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
+# strings = ["big", "little", "good", "bad"]
+# p no_b_ginning = strings.select{|word| word[0].downcase != "b"}
+
+#  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
+# items = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+# p cheap = items.select{|item| item[:price] < 10}
+
+# 10. Start with an array of numbers and create a new array with only the odd numbers.
+#     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
+# numbers = [2, 4, 5, 1, 8, 9, 7]
+# p odd = numbers.select{|number| number.odd?}
+# p odd = numbers.select{|number| number % 2 != 0}
+
+
+# SOLUTIONS (using while loop): https://gist.github.com/peterxjang/7de16ed43ea506e98df3fa15074b84f8
+# SOLUTIONS (using .each shortcut): https://gist.github.com/peterxjang/a702894841c7018ed8c127b647ae21f8
+# SOLUTIONS (using .select shortcut): https://gist.github.com/peterxjang/b8c8fb8b77b2cae7bb9cc62a3a434761
